ab32f08d5fe87cbf862e3a1088a05d060ae35628
olegk <kbryzhenyukpc@gmail.com>
2024-11-14 22:56:47 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Quote;
+
+    internal class Program
+    {
+    static void Main()
+    {
+        Console.WriteLine("It's easy to win forgiveness for being wrong;");
+        Console.WriteLine("being right is what gets you into real trouble.");
+        Console.WriteLine("Bjarne Stroustrup");
+    }
+}
